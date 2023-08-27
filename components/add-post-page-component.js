4358ef9ca90389ba6aa82e32312fadbbe812a465
StasGushchin <stas031094@gmail.com>
2023-08-27 20:41:23 +0300

    post correct

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,23 +1,47 @@
+import { renderHeaderComponent } from "./header-component.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
-      <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
+      <div class="header-container">
+  </div>
+  
+  </div>
+      <div class="form">
+        <h3 class="form-title">Добавить пост</h3>
+        <div class="form-inputs">
+          <div class="upload-image-container">
+  <div class="upload-image">
+         
+  </div>
+  </div>
+          <label>
+            Опишите фотографию:
+            <textarea class="input textarea" rows="4" id="input-id"></textarea>
+            </label>
+            <button class="button" id="add-button">Добавить</button>
+        </div>
+      </div>
     </div>
   `;
 
     appEl.innerHTML = appHtml;
 
+    
+
     document.getElementById("add-button").addEventListener("click", () => {
+      const postDescription = document.getElementById("input-id");
       onAddPostClick({
-        description: "Описание картинки",
+        description: postDescription.value,
         imageUrl: "https://image.png",
       });
     });
   };
 
   render();
+  renderHeaderComponent({
+    element: document.querySelector(".header-container"),
+  });
 }
